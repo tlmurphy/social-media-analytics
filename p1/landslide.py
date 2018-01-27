d2e70bf046417d978840ef4dd6ab4b18f0e39932
Trevor Murphy <murphtrevor@gmail.com>
2018-01-27 16:43:41 -0600

    Switch to python2 and update readme

--- p1/landslide.py
+++ p1/landslide.py
@@ -2,26 +2,24 @@
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from datetime import timezone, datetime, timedelta
+from datetime import datetime, timedelta
+import rfc3339
+import os
 
-# DEVELOPER_KEY comes from a google cloud project.
-DEVELOPER_KEY = 'AIzaSyCphGCNKcetS7iXLVBDCnRTVkdX01LugSg'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
-END_DATE = (datetime.now(timezone.utc).astimezone()).isoformat()
-START_DATE = (datetime.now(timezone.utc).astimezone() - timedelta(days=1)).isoformat()
-CLIENT = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-               developerKey=DEVELOPER_KEY)
+END_DATE = datetime.now()
+START_DATE = datetime.now() - timedelta(days=1)
 
 
-def youtube_search():
-    search_response = CLIENT.search().list(
+def youtube_search(client):
+    search_response = client.search().list(
         q='landslide',
         part='id,snippet',
         order='viewCount',
         type='video',
-        publishedAfter=START_DATE,
-        publishedBefore=END_DATE,
+        publishedAfter=rfc3339.rfc3339(START_DATE),
+        publishedBefore=rfc3339.rfc3339(END_DATE),
         maxResults=10
     ).execute()
 
@@ -30,15 +28,15 @@ def youtube_search():
     for search_result in search_response.get('items', []):
         if search_result['id']['kind'] == 'youtube#video':
             videos.append('{}\t{}\t{}'.format(search_result['id']['videoId'],
-                                              search_result['snippet']['title'],
-                                              get_viewCount(search_result['id']['videoId'])))
+                                              search_result['snippet']['title'].encode('utf-8'),
+                                              get_viewCount(client, search_result['id']['videoId'])))
 
-    print('{}\t{}\n'.format(START_DATE, END_DATE))
-    print('\n'.join(videos))
+    print '{}\t{}\n'.format(START_DATE, END_DATE)
+    print '\n'.join(videos)
 
 
-def get_viewCount(vid_id):
-    response = CLIENT.videos().list(
+def get_viewCount(client, vid_id):
+    response = client.videos().list(
         part='statistics',
         id=vid_id
     ).execute()
@@ -47,6 +45,10 @@ def get_viewCount(vid_id):
 
 if __name__ == '__main__':
     try:
-        youtube_search()
-    except HttpError as e:
+        client = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                       developerKey=os.environ['YT_DEV_KEY'])
+        youtube_search(client)
+    except KeyError:
+        print 'Please set your YT_DEV_KEY environment variable.'
+    except HttpError, e:
         print('An HTTP error {} occurred:\n{}'.format(e.resp.status, e.content))
