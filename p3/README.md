20f3fb4bce35cfb685054d469f32bfe3a6101ecb
Trevor Murphy <murphtrevor@gmail.com>
2018-03-03 17:36:58 -0600

    Add google maps tool day as well as project 3

---
+++ p3/README.md
@@ -0,0 +1,9 @@
+1. Install packages
+```bash
+pip install gensim
+pip install scikit-learn
+```
+
+2. Download `evaluation_dataset`, `training_dataset`, and `GoogleNews-vectors-negative300.bin` into the current directory (files must be named as shown).
+
+3. Execute `python dta.py`
---
+++ p3/dta.py
@@ -0,0 +1,133 @@
+from time import time
+import json
+from re import findall
+import numpy as np
+from gensim.models.keyedvectors import KeyedVectors
+from sklearn import svm
+
+
+class DeepTextAnalyzer(object):
+    def __init__(self, word2vec_model):
+        """
+        Construct a DeepTextAnalyzer using the input Word2Vec model
+        :param word2vec_model: a trained Word2Vec model
+        """
+        self._model = word2vec_model
+
+    def txt2vectors(self, txt, is_html):
+        """
+        Convert input text into an iterator that returns the corresponding vector representation of each
+        word in the text, if it exists in the Word2Vec model
+        :param txt: input text
+        :param is_html: if True, then extract the text from the input HTML
+        :return: iterator of vectors created from the words in the text using the Word2Vec model.
+        """
+        words = txt2words(txt)
+        words = [w for w in words if w in self._model]
+        if len(words) != 0:
+            for w in words:
+                yield self._model[w]
+
+    def txt2avg_vector(self, txt, is_html):
+        """
+        Calculate the average vector representation of the input text
+        :param txt: input text
+        :param is_html: is the text is a HTML
+        :return the average vector of the vector representations of the words in the text
+        """
+        vectors = self.txt2vectors(txt, is_html=is_html)
+        vectors_sum = next(vectors, None)
+        if vectors_sum is None:
+            return None
+        count = 1.0
+        for v in vectors:
+            count += 1
+            vectors_sum = np.add(vectors_sum, v)
+
+        # calculate the average vector and replace +infy and -inf with numeric values
+        avg_vector = np.nan_to_num(vectors_sum / count)
+        return avg_vector
+
+
+def txt2words(inputString):
+    return findall(r'(?u)\w+', inputString)
+
+
+def w2v_load_model(fname):
+    return KeyedVectors.load_word2vec_format(fname, binary=True)
+
+
+def load_tweets(fname):
+    """Place tweets into a list, loading from a JSON file"""
+    tweets = []
+    for line in open(fname):
+        tweets.append(json.loads(line))
+    return tweets
+
+
+def w2v_vector(dta, text):
+    vector = dta.txt2avg_vector(text, is_html=False)
+    if vector is None:
+        return
+    return [0.0 if v is None else str(v) for v in vector]
+
+
+def generate_report(metrics):
+    with open('report.txt', 'w') as f:
+        f.write('True positives: {}\tFalse positives: {}\tTrue negatives: {}\tFalse negatives: {}\n'.format(
+            metrics['tp'], metrics['fp'], metrics['tn'], metrics['fn']))
+        f.write('Precision: {}\tRecall: {}\tF1-score: {}\n'.format(
+            metrics['precision'], metrics['recall'], metrics['f1']))
+
+
+if __name__ == '__main__':
+    # Load tweets and model
+    t = time()
+    print 'loading tweets, please wait...'
+    trained_tweets = load_tweets('training_dataset')
+    eval_tweets = load_tweets('evaluation_dataset')
+    print 'Time taken {}'.format(time() - t)
+    t = time()
+    print 'loading w2v model, please wait...'
+    model = w2v_load_model('GoogleNews-vectors-negative300.bin')
+    print 'Time taken {}'.format(time() - t)
+
+    # Build vectors and classify
+    dta = DeepTextAnalyzer(model)
+    clf = svm.SVC()
+    X = []  # samples
+    y = []  # features
+    for tweet in trained_tweets:
+        vector = w2v_vector(dta, tweet['text'])
+        if vector is not None:  # ValueError: setting an array element with a sequence :')
+            X.append(vector)
+            y.append(tweet['label'])
+    clf.fit(X, y)
+
+    # Predict
+    for tweet in eval_tweets:
+        tweet['prediction'] = clf.predict([w2v_vector(dta, tweet['text'])])[0]
+
+    metrics = {'tp': 0, 'fp': 0, 'tn': 0, 'fn': 0,
+               'precision': 0, 'recall': 0, 'f1': 0}
+    for tweet in eval_tweets:
+        actual_label = tweet['label']
+        prediction = tweet['prediction']
+        if actual_label == prediction:
+            if prediction == 'relevant':
+                metrics['tp'] += 1
+            else:
+                metrics['tn'] += 1
+        else:
+            if prediction == 'relevant':
+                metrics['fp'] += 1
+            else:
+                metrics['fn'] += 1
+    metrics['precision'] = float(metrics['tp']) / (metrics['tp'] + metrics['fp'])
+    metrics['recall'] = float(metrics['tp']) / (metrics['tp'] + metrics['fn'])
+    metrics['f1'] = 2 * ((metrics['precision'] * metrics['recall']) /
+                         (metrics['precision'] + metrics['recall']))
+
+    generate_report(metrics)
+
+    print 'Check report.txt for results.'
